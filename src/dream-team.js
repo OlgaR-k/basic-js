71bc910d64f9cf84528deb7ea12c3b1a8a99329b
Olga Romanchik <volha.ramanchyk@gmail.com>
2020-10-02 12:21:52 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let teamName = '';
+  let sortArray = deleteSpace(members).sort();
+  members.sort();
+
+  for (let elem of sortArray) {
+    teamName += elem[0];
+  }
+
+  return teamName;
 };
+
+function deleteSpace(arr) {
+  let res = [];
+  for (let elem of arr) {
+    if (typeof (elem) === 'string') {
+      res.push(elem.replace(/\s+/, '').toUpperCase());
+    }
+  }
+  return res;
+}
+
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( /* arr */ ) {
+   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  /*
+  if (!Array.isArray(arr)) {
+    throw new Error('arr is not Array');
+  }
+
+  const discard_next = `--discard-next`;
+  const discard_prev = `--discard-prev`;
+  const double_next = `--double-prev`;
+  const double_prev = `--double-prev`;
+
+  let res = [];
+  if ( arr.indexOf( discard_next ) !== -1 ) {
+    return get_discard_next(arr, arr.indexOf( discard_next ) );
+  } else if ( arr.indexOf( discard_prev ) !== -1 ) {
+    return get_discard_prev(arr, arr.indexOf( discard_prev ) );
+  } else if ( arr.indexOf( double_next ) !== -1 ) {
+    return get_double_next(arr, arr.indexOf( double_next ) );
+  } else if ( arr.indexOf( double_prev ) !== -1 ) {
+    return get_double_prev(arr, arr.indexOf( double_prev ) );
+  }
+  return res;
+  */
 };
+
+function get_discard_next( arr, ind ) {
+  let res = arr.concat();
+  res.splice(ind, 2);
+  return res;
+}
+function get_discard_prev( arr, ind ) {
+  let res = arr.concat();
+  res.splice(ind - 1, 2);
+  return res;
+}
+function get_double_next( arr, ind ) {
+  let res = arr.concat();
+  res.splice(ind, 1);
+  return res.map( (elem, index) => index === ind ? elem * 2 : elem );
+}
+function get_double_prev( arr, ind ) {
+  let res = arr.concat();
+  res.splice(ind, 1);
+  return res.map( (elem, index) => index - 1 === ind ? elem * 2 : elem );
+}
