8cd65fcd310cf07f71eab8ded5d2fa3fd2d61731
Olga Romanchik <volha.ramanchyk@gmail.com>
2020-10-02 16:46:05 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if ( !date ) {
+    return 'Unable to determine the time of year!';
+  } else if ( Object.prototype.toString.call(date) !== '[object Date]' ) {
+    throw new TypeError('Type is not Date');
+  }
+  const spring = 'spring';
+  const summer = 'summer';
+  const autumn  = 'autumn';
+  const winter = 'winter';
+
+  const month = date.getMonth();
+
+  if ( month <= 1 || month === 11 ) {
+    return winter;
+  } else if ( month <= 4 ) {
+    return spring;
+  } else if ( month <= 7 ) {
+    return summer;
+  } else {
+    return autumn;
+  }
 };
