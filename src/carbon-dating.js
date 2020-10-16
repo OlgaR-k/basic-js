215e3882e229c8eaf1d3f889701afab13f0299c7
Olga Romanchik <volha.ramanchyk@gmail.com>
2020-10-17 01:25:17 +0300

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,12 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
+const ln2 = 0.639;
 
-module.exports = function dateSample( /*sampleActivity*/ ) {
-  throw new CustomError('Not implemented');
-  // if ( !sampleActivity || isNaN(sampleActivity) ) {
-  //   return false;
-  // }
-
-  // return true;
+module.exports = function dateSample( sampleActivity ) {
+  if ( isNaN(sampleActivity) || typeof(sampleActivity) !== 'string' || sampleActivity === '' || !!sampleActivity) {
+    return false;
+  }
+  return Math.ceil(Math.log( MODERN_ACTIVITY / sampleActivity) / ln2 * HALF_LIFE_PERIOD);
 };
